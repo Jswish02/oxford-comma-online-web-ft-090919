61b0aa7426b1f356b35f587a4718b8b66ce7c93a
Jonah Brown <jswish02@gmail.com>
2019-09-13 18:17:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,4 @@
-#def oxford_comma(array)
-#array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
- array.shift 
-end
+
 def oxford_comma(array)
   if array.length == 2
     array[-2] << " and "
